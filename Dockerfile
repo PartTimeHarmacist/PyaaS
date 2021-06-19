c6c7d6d4d8604946b9d2c2201a30a6c693fc4c17
harmacist <harmacistant@icloud.com>
2021-06-18 22:21:56 -0500

    Inclusion of docker module and api

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,8 @@ FROM python:latest
 WORKDIR /opt/pyfaas
 
 COPY src /opt/pyfaas
+COPY requirements.txt /tmp/requirements.txt
+
+RUN python -m pip install -r /tmp/requirements.txt
 
 CMD python -m pyfaas.pyfaas
--- src/pyfaas/pyfaas.py
+++ src/pyfaas/pyfaas.py
@@ -1,12 +1,20 @@
+import docker
+
+
 # noinspection SpellCheckingInspection
 class PyfaaS:
     def __init__(self):
-        pass
+        self.docker_client = docker.from_env()
 
     @staticmethod
     def hello(subj='world'):
         print(f"Hello, {subj}!")
 
+    def list_containers(self):
+        print(self.docker_client.containers())
+
 
 if __name__ == "__main__":
     PyfaaS.hello()
+    fn = PyfaaS()
+    fn.list_containers()
