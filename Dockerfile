e39a585dfc32f2875d6686fab8d546b111ff491c
harmacist <harmacistant@icloud.com>
2021-06-19 19:37:38 -0500

    Added six requirement to requirements.txt to account for broken docker package requirements.

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR /opt/pyfaas
 COPY src /opt/pyfaas
 COPY requirements.txt /tmp/requirements.txt
 
-RUN python -m pip install -r /tmp/requirements.txt
+RUN python -m pip install --no-cache-dir -r /tmp/requirements.txt
 
 CMD python -m pyfaas.pyfaas
--- src/pyfaas/pyfaas.py
+++ src/pyfaas/pyfaas.py
@@ -1,17 +1,27 @@
 import docker
 
 
+PYFAAS_VERSION = "0.0.1"
+
+
 # noinspection SpellCheckingInspection
 class PyfaaS:
     def __init__(self):
         self.docker_client = docker.from_env()
+        self.container_default_labels = {"PyfaaS_version": self.get_version(),
+                                         "PyfaaS_schedule": "@none",
+                                         "PyfaaS_source": "local"}
 
     @staticmethod
     def hello(subj='world'):
         print(f"Hello, {subj}!")
 
+    @staticmethod
+    def get_version():
+        return PYFAAS_VERSION
+
     def list_containers(self):
-        print(self.docker_client.containers())
+        return self.docker_client.containers.list(filters={'label': self.container_default_labels.keys()})
 
 
 if __name__ == "__main__":
