921a2d67bf088282a5d6701e0235d317af94d379
harmacist <harmacistant@icloud.com>
2021-06-20 01:05:48 -0500

    Modified hello function to utilize docker containers instead of printing; added more example functionality to the __main__ portion of the pyfaas.py script.

--- src/pyfaas/pyfaas.py
+++ src/pyfaas/pyfaas.py
@@ -2,6 +2,7 @@ import docker
 
 
 PYFAAS_VERSION = "0.0.1"
+PYFAAS_NAME = "PyfaaS"
 
 
 # noinspection SpellCheckingInspection
@@ -12,19 +13,39 @@ class PyfaaS:
                                          "PyfaaS_schedule": "@none",
                                          "PyfaaS_source": "local"}
 
-    @staticmethod
-    def hello(subj='world'):
-        print(f"Hello, {subj}!")
+    def hello(self, subj='world'):
+        self.docker_client.containers.run("alpine", f"echo hello, {subj}!  This is {self}",
+                                          labels=fn.container_default_labels,
+                                          name="pyfaas_hello",
+                                          remove=True,
+                                          detach=True)
 
     @staticmethod
     def get_version():
         return PYFAAS_VERSION
 
-    def list_containers(self):
-        return self.docker_client.containers.list(filters={'label': self.container_default_labels.keys()})
+    @staticmethod
+    def get_name():
+        return PYFAAS_NAME
+
+    def __str__(self):
+        return f"{self.get_name()} - v{self.get_version()} [{len(self.list_containers())} Containers]"
+
+    def list_containers(self, list_all=False):
+        return self.docker_client.containers.list(filters={'label': list(self.container_default_labels.keys())},
+                                                  all=list_all)
+
+    def clean_up_containers(self, running=False):
+        for container in self.list_containers(list_all=True):
+            container.remove()
 
 
 if __name__ == "__main__":
-    PyfaaS.hello()
     fn = PyfaaS()
-    fn.list_containers()
+    fn.hello(subj='Docker')
+    print(fn)
+    fn.docker_client.containers.run("alpine", "echo hello, world!",
+                                    labels=fn.container_default_labels,
+                                    detach=True)
+    print(','.join([c.attrs['Name'] for c in fn.list_containers(list_all=True)]))
+    fn.clean_up_containers(running=True)
